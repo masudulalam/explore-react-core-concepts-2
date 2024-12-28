ce4613126c8cb56b1ee438c928b4a47e43ac2823
masudulalam <masuudulalam@gmail.com>
2024-12-28 23:36:45 +0600

    explore how to add event handler in react

--- explore-react-core-concepts-part2/src/App.jsx
+++ explore-react-core-concepts-part2/src/App.jsx
@@ -10,12 +10,17 @@ function App() {
     alert('button clicked 2');
   }
 
+  const addToFive = (num) => {
+    alert(num + 5);
+  }
+
   return (
     <>
       <h3>React Core Concepts 2</h3>
       <button onClick={handleClick}>Click Me!</button>
       <button onClick={handleClick2}>Click Me 2!</button>
       <button onClick={() => {alert('Click me 3 is clicked')}}>Click Me 3!</button>
+      <button onClick = {() => addToFive(5)}>Add Five</button>
     </>
   )
 }
